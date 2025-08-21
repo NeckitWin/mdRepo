e00e1a4c43ef527be203f1e24db4920a8f13a81d
NeckitWin <neckit.v@gmail.com>
2025-08-21 18:34:06 +0200

    feat: add logic for add/delete details profile

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -34,6 +34,20 @@ const Profile = () => {
         });
     }
 
+    const deleteDetail = (index: number) => {
+        setTempUser(prev => {
+            const newDetails = prev.details.filter((_, i) => i !== index);
+            return { ...prev, details: newDetails };
+        });
+    }
+
+    const addDetail = () => {
+        setTempUser(prev => ({
+            ...prev,
+            details: [...prev.details, { label: "", value: "" }]
+        }));
+    }
+
     const saveProfile = () => {
         setEditMode(false);
         setUser(tempUser);
@@ -62,7 +76,7 @@ const Profile = () => {
                         <li key={index} className="flex gap-2 items-center">
                             <input onChange={(e)=>changeDetail(index, "label", e.target.value)} className="font-semibold text-[#A3A3A3] text-2xl w-[193px] border-b border-white focus:outline-none" value={detail.label} />
                             <input onChange={(e)=>changeDetail(index, "value", e.target.value)} type="text" value={detail.value} className="text-2xl text-[#EAEBF6] bg-transparent border-b border-white focus:outline-none" />
-                            <button className="w-6 h-6 duration-200 fill-white/20 hover:fill-white/50 cursor-pointer">
+                            <button onClick={()=>deleteDetail(index)} className="w-6 h-6 duration-200 fill-white/20 hover:fill-white/50 cursor-pointer">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M232.7 69.9C237.1 56.8 249.3 48 263.1 48L377 48C390.8 48 403 56.8 407.4 69.9L416 96L512 96C529.7 96 544 110.3 544 128C544 145.7 529.7 160 512 160L128 160C110.3 160 96 145.7 96 128C96 110.3 110.3 96 128 96L224 96L232.7 69.9zM128 208L512 208L512 512C512 547.3 483.3 576 448 576L192 576C156.7 576 128 547.3 128 512L128 208zM216 272C202.7 272 192 282.7 192 296L192 488C192 501.3 202.7 512 216 512C229.3 512 240 501.3 240 488L240 296C240 282.7 229.3 272 216 272zM320 272C306.7 272 296 282.7 296 296L296 488C296 501.3 306.7 512 320 512C333.3 512 344 501.3 344 488L344 296C344 282.7 333.3 272 320 272zM424 272C410.7 272 400 282.7 400 296L400 488C400 501.3 410.7 512 424 512C437.3 512 448 501.3 448 488L448 296C448 282.7 437.3 272 424 272z" /></svg>
                             </button>
                         </li>
@@ -73,6 +87,11 @@ const Profile = () => {
                         </li>
                     )
                 })}
+                {editMode && <li>
+                    <button className="cursor-pointer transition w-full text-neutral-200 hover:text-neutral-400" onClick={addDetail}>
+                        Add Detail
+                    </button>
+                </li>}
             </ul>
         </section>
     )
