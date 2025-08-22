99da635129f4068044b285330c995580b1c26671
NeckitWin <neckit.v@gmail.com>
2025-08-22 22:10:46 +0200

    fix: hook initialization

--- client/src/hooks/useNotes.ts
+++ client/src/hooks/useNotes.ts
@@ -1,7 +1,17 @@
 import { useState } from "react";
 import type { Note } from "../types/note";
 
-export const useNotes = (initialNotes: Note[]) => {
+const initialNotes: Note[] = [
+  {
+    id: "1",
+    title: "Example Note",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos nostrum exercitationem repellat, voluptatem laboriosam at corrupti commodi nesciunt quia voluptates vitae, officia iste saepe explicabo aliquid maxime omnis, excepturi qui. Reprehenderit exercitationem distinctio vero nemo neque minima consequuntur nulla sequi quidem a enim sed, incidunt quod accusamus voluptas nihil ipsa magnam velit sapiente nesciunt, dicta soluta illum commodi. Consequuntur, magnam.",
+    date: "2025-08-20",
+    tags: ["Test", "Test2", "Test3"]
+  }
+];
+
+export const useNotes = () => {
   const [notesList, setNotesList] = useState<Note[]>(initialNotes);
 
   const addNote = () => {
