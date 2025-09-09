6fd6fa327fbdcabcb0594dab1de8cee9ad7f6e07
NeckitWin <neckit.v@gmail.com>
2025-09-09 21:24:54 +0200

    feat: edit and delete note button

--- client/src/components/NoteList.tsx
+++ client/src/components/NoteList.tsx
@@ -1,13 +1,18 @@
 import type { Note } from "../types/note";
 import NoteItem from "./NoteItem";
 
-const NoteList = ({ notesList }: { notesList: Note[] }) => {
+interface NoteListProps {
+    notesList: Note[];
+    deleteNote: (id: string) => void;
+}
+
+const NoteList = ({ notesList, deleteNote }: NoteListProps) => {
     return (
         <section className="bg-[#1E1A1A] rounded-[10px] shadow-sm shadow-neutral-700 p-6">
             <ul className="space-y-4 h-[700px] overflow-y-auto">
                 {notesList.map((note) => (
                     <li key={note.id} >
-                        <NoteItem note={note} />
+                        <NoteItem note={note} deleteNote={deleteNote} />
                     </li>
                 ))}
             </ul>
