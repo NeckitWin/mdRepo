92a0f9d3fda57c74c2f8659e624c5ac321ca9b8a
NeckitWin <neckit.v@gmail.com>
2025-09-09 20:51:27 +0200

    feat: add deleteNote function to remove note by id

--- client/src/hooks/useNotes.ts
+++ client/src/hooks/useNotes.ts
@@ -2,36 +2,41 @@ import { useState } from "react";
 import type { Note } from "../types/note";
 
 const initialNotes: Note[] = [
-  {
-    id: "1",
-    title: "Example Note",
-    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos nostrum exercitationem repellat, voluptatem laboriosam at corrupti commodi nesciunt quia voluptates vitae, officia iste saepe explicabo aliquid maxime omnis, excepturi qui. Reprehenderit exercitationem distinctio vero nemo neque minima consequuntur nulla sequi quidem a enim sed, incidunt quod accusamus voluptas nihil ipsa magnam velit sapiente nesciunt, dicta soluta illum commodi. Consequuntur, magnam.",
-    date: "2025-08-20",
-    tags: ["Test", "Test2", "Test3"]
-  }
+    {
+        id: "1",
+        title: "Example Note",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos nostrum exercitationem repellat, voluptatem laboriosam at corrupti commodi nesciunt quia voluptates vitae, officia iste saepe explicabo aliquid maxime omnis, excepturi qui. Reprehenderit exercitationem distinctio vero nemo neque minima consequuntur nulla sequi quidem a enim sed, incidunt quod accusamus voluptas nihil ipsa magnam velit sapiente nesciunt, dicta soluta illum commodi. Consequuntur, magnam.",
+        date: "2025-08-20",
+        tags: ["Test", "Test2", "Test3"]
+    }
 ];
 
 export const useNotes = () => {
-  const [notesList, setNotesList] = useState<Note[]>(initialNotes);
+    const [notesList, setNotesList] = useState<Note[]>(initialNotes);
 
-  const addNote = () => {
-    const newNote: Note = {
-      id: crypto.randomUUID(),
-      title: "New Note",
-      content: "",
-      date: new Date().toISOString().split("T")[0],
-      tags: []
+    const addNote = () => {
+        const newNote: Note = {
+            id: crypto.randomUUID(),
+            title: "New Note",
+            content: "",
+            date: new Date().toISOString().split("T")[0],
+            tags: []
+        };
+        setNotesList((prev) => [...prev, newNote]);
     };
-    setNotesList((prev) => [...prev, newNote]);
-  };
 
-  const deleteAllNotes = () => {
-    setNotesList([]);
-  };
+    const deleteAllNotes = () => {
+        setNotesList([]);
+    };
 
-  return {
-    notesList,
-    addNote,
-    deleteAllNotes
-  };
+    const deleteNote = (id: string) => {
+        setNotesList((prev) => prev.filter(note => note.id !== id));
+    };
+
+    return {
+        notesList,
+        addNote,
+        deleteAllNotes,
+        deleteNote
+    };
 };
